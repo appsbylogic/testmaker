dc7b89a8c1039871f5dce1473203058d281ef559
Fmaldonado6 <f_maldonado6@hotmail.com>
2019-05-28 23:21:22 -0500

    Final 2

--- js/test.js
+++ js/test.js
@@ -4,34 +4,37 @@ var respuestas
 var preguntas
 var instances
 var instance
+var modal
+var modal2
+var score = 0
+var first = true
+var skip =false;
+
 $(document).ready(function(){
     $('.modal').modal();
     $('.collapsible').collapsible();
+    
+    if(localStorage.getItem('storage')){
+        
+        $('.mainMenu').html(localStorage.getItem('storage'))
+
+        
+
+    }
+   
   });
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelector('.collapsible');
     instances = M.Collapsible.init(elems);
     instance = M.Collapsible.getInstance(elems);
+    var elems1 = document.querySelector('#modalName');
+    var instances = M.Modal.init(elems1);
+    modal = M.Modal.getInstance(elems1);
+    var elems2 = document.querySelector('#modalFinal');
+    var instances = M.Modal.init(elems2);
+    modal2 = M.Modal.getInstance(elems2);
   });
 
-function leer(preguntas, respuestas){
-    $.get(preguntas, function(data) {
-        lines = data.split("\n");
-        console.log(lines.length)
-        
-            
-     });
-    
-     $.get(respuestas, function(data) {
-        answers = data.split("\n");
-        
-        console.log(answers.length)
-        
-     });
-
-     
-
-}
 
 function inputRes(respuestas){
 
@@ -45,12 +48,20 @@ function inputPreg(preguntas){
 
 }
 
+var cuestionario
+
 function submit(){
 
     
 
-    var preguntas = document.getElementById('input').files[0];
-    var respuestas = document.getElementById('input2').files[0];
+    
+
+    preguntas= document.getElementById('input').files[0]
+    respuestas = document.getElementById('input2').files[0]
+    
+    
+    
+
     if (preguntas) {
         // create reader
         var reader = new FileReader();
@@ -69,11 +80,7 @@ function submit(){
 
                 }
 
-                
-
-
             }
-
            
             
         };
@@ -96,50 +103,81 @@ function submit(){
                     
                     answers.splice(i,1)
 
-                }
-
-                
+                }           
 
 
             }
-            console.log(answers)
+           
             
         };
-    }
-
-    
+    } 
 
     if(preguntas && respuestas){
 
-        instance.close()
-        $('.test').removeClass('disappear')
-
-
-    setTimeout(function(){
-
-        next()
-
-     },1000)
-
+        
+    
+        modal.open()
     }
+ 
+}
+
+function create(){
+    cuestionario = [lines,answers]
+
+
+    console.log(cuestionario)
+
+    localStorage.setItem(preguntas.name, JSON.stringify(cuestionario))
+
+
+    instance.close()
+    //$('.test').removeClass('disappear')
+
+    var name = $('#testName').val()
 
     
 
+    $('.tests').append('<div class="card scale-transition" ><div class="card-content">\
+    <div class="row"><div class="col s12">\
+    <a class="btn-floating waves-effect waves-light delete" id="back">\
+    <i class="material-icons del">close</i>></a></div></div><div class="row"><br><div class="col s12"><h5 contenteditable>' + name + '</h5></div></div>\
+    <div class="row"><div class="col s12">\
+    <a class="btn-floating waves-effect waves-light go" id="back" onclick="next('+ "'" + document.getElementById('input').files[0].name + "'" +')"><i class="material-icons del">chevron_right</i>></a>\
+    </div></div>')
+
+    $('.tests').removeClass('disappear')
+
+    localStorage.setItem('storage', $('.mainMenu').html())
 
     
+    modal.close()
 
-     
-     
-
+    
 }
 
 var correcta 
 
-var pregunta = localStorage.getItem('pregunta') ? parseInt(localStorage.getItem('pregunta')) : 0
- function next(){
+var cuestName
+
+var pregunta = 0
+ function next(name){
+
+    first = true
+
+    cuestName = name
+     $('.mainMenu').addClass('disappear')
+     $('.test').removeClass('disappear')
     $('#correct').html('')
     $('.radio').prop('checked', false)
     $('#nextQ').addClass('disabled')
+
+    cuestionario = localStorage.getItem(name)
+
+    lines = JSON.parse(cuestionario)[0]
+
+    answers = JSON.parse(cuestionario)[1]
+
+    console.log()
 
     correcta = Math.floor(Math.random()*3)
     
@@ -190,7 +228,8 @@ var pregunta = localStorage.getItem('pregunta') ? parseInt(localStorage.getItem(
 
 function nextQuestion(){
 
-    console.log(pregunta)
+    console.log(answers.length)
+    
 
     if(pregunta<answers.length){
         pregunta +=1
@@ -198,7 +237,25 @@ function nextQuestion(){
     
         localStorage.setItem('pregunta', pregunta)
     
-        next()
+        next(cuestName)
+
+    }
+    
+    if(pregunta==answers.length){
+
+        var total = answers.length
+        console.log(score)
+        if(!skip){
+            $('#score').html(score+'/'+total)
+
+        }else{
+
+            $('#score').html('You skipped questions! You need to answer all the questions in order to get a score')
+
+        }
+        
+
+        modal2.open()
 
     }
 
@@ -210,12 +267,18 @@ function nextQuestion(){
     console.log(correcta)
     res = $('input[name=group1]:checked').val()
     console.log(res)
+
+    if(res == correcta&&first){
+        score+=1
+        
+    }
     if(res == correcta){
+        
         $('#correct').html('check')
         $('#nextQ').removeClass('disabled')
         
     }else{
-
+        first=false
         $('#correct').html('close')
 
     }
@@ -224,19 +287,50 @@ function nextQuestion(){
 
  function reset(){
     pregunta = 0
+    skip =false;
 
-    localStorage.setItem('pregunta', pregunta)
 
-    next()
+    next(cuestName)
 
 
  }
 
 function goTo(){
+
+    skip = true
     
     pregunta = $('#questionNumber').val()-1
-    localStorage.setItem('pregunta', pregunta)
+    
 
-    next()
+    next(cuestName)
 }
 
+function backMenu(){
+    $('.mainMenu').removeClass('disappear')
+     $('.test').addClass('disappear')
+     skip =false;
+
+
+}
+
+$(document).on('click','.delete',function(e){
+	
+
+    console.log(e)
+    
+    $(e.target.parentNode.parentNode.parentNode.parentNode.parentNode).addClass('scale-transition')
+	
+	$(e.target.parentNode.parentNode.parentNode.parentNode.parentNode).addClass('scale-out')
+
+	window.setTimeout(function(){
+        e.target.parentNode.parentNode.parentNode.parentNode.parentNode.remove()
+        
+        localStorage.setItem('storage', $('.mainMenu').html())
+
+	},400)
+
+    
+
+
+
+})
